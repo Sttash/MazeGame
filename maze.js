c5fb4087d8c2d2d877451b8289c88f6ba55ff73c
Иван <risk94@outlook.com>
2018-11-23 23:26:16 +0500

    some_additions_1

--- maze.js
+++ maze.js
@@ -1,15 +1,19 @@
 // Определяем глобальные переменные для холста и контекста 
-var canvas;
-var context;
+var canvas,
+    context,
 
 // Отслеживаем текущую позицию значка
-var iconX = 0;
-var iconY = 0;
+    iconX = 0,
+    iconY = 0,
 
 // Скорость перемещения значка
-var dx = 0;
-var dy = 0;
+    dx = 0,
+    dy = 0,
 
+// коды клавиш (wasd и стрелки)
+    movementKeys = [ 38, 87, 40, 83, 37, 65, 39, 68 ];
+
+// стартовые операции
 window.onload = function() {
   // Подготавливаем холст
   canvas = document.getElementById("canvas");
@@ -18,15 +22,19 @@ window.onload = function() {
   // Рисуем фон лабиринта
   drawMaze("maze.png", 268, 5);
 
-  // При нажатии клавиши вызываем функцию processKey(e)
-  // при отпускании вызывается processUpKey(e)
+  // При нажатии клавиши вызываем функцию processKey(e) для перемещения
+  // при отпускании вызывается processUpKey(e) для остановки значка
   window.onkeydown = processKey;
   window.onkeyup = processUpKey;
 }
 
-// Таймер, включающий и отключающий новый лабиринт в любое время
-var timer;
+// Таймеры и индикаторы состояний 
+var timer,
+    timerClock,
+    isClockStarted = false,
+    isFinished = false;
 
+// Отрисовка 
 function drawMaze(mazeFile, startingX, startingY) {
   // Остановить таймер (если запущен)
   clearTimeout(timer);
@@ -63,25 +71,33 @@ function drawMaze(mazeFile, startingX, startingY) {
 // Обработка нажатия клавиш
 function processKey(e) {
   // Если значок находится в движении, останавливаем его
-  dx = 0;
-  dy = 0;
+  //dx = 0;
+  //dy = 0;
+
+  // Таймер запускается только после перемещения значка
+  if (isClockStarted == false && isFinished == false
+    && movementKeys.includes(e.keyCode)) {
+    isClockStarted = true;
+    timerClock = setInterval(tickTime, 1000);
+  }
 
   // Если нажата стрелка вверх, начинаем двигаться вверх
-  if (e.keyCode == 38) {dy = -1;}
+  if (e.keyCode == 38 || e.keyCode == 87) {dy = -1;}
   // Вниз
-  if (e.keyCode == 40) {dy = 1;}
+  if (e.keyCode == 40 || e.keyCode == 83) {dy = 1;}
   // Влево
-  if (e.keyCode == 37) {dx = -1;}
+  if (e.keyCode == 37 || e.keyCode == 65) {dx = -1;}
   // Вправо
-  if (e.keyCode == 39) {dx = 1;}
+  if (e.keyCode == 39 || e.keyCode == 68) {dx = 1;}
 }
 
 // Если клавиша отпущена, движение останавливается
-function processUpKey(e) {
+function processUpKey() {
   dx = 0;
   dy = 0;
 }
 
+// проверка столкновения со стеной
 function checkForCollision() {
   // Перебираем все пикселы и инвертируем их цвет
   var imgData = context.getImageData(iconX-1, iconY-1, 15+2, 15+2);
@@ -107,7 +123,7 @@ function checkForCollision() {
   return false;
 }
 
-
+// отрисовка значка и следа
 function drawFrame() {
   // Обновляем кадр только если значок движется
   if (dx != 0 || dy != 0) {
@@ -137,7 +153,9 @@ function drawFrame() {
     // Проверяем дошел ли пользователь до финиша.
 	  // Если дошел, то выводим сообщение
     if (iconY > (canvas.height - 17)) {
-      alert("Ты победил!");
+      alert("Ты победил!\nТвоё время: " + timerText.innerHTML);
+      resetTimer();
+      isFinished = true;
       return;
     }
   }
@@ -146,10 +164,44 @@ function drawFrame() {
   timer = setTimeout(drawFrame, 10);
 }
 
-function loadEasy() {
-  drawMaze('easy_maze.png', 5, 5);
+var minTime = 0, // минуты
+    secTime = 1; // секунды
+var timerText = document.getElementById("timer"); // label для отображения таймера
+
+// обновление значения таймера (раз в секунду)
+function tickTime() {
+  // добавление "0" перед значением секунды, если оно < 10
+  timerText.innerHTML =
+    (secTime < 10)
+    ? `${minTime}:0${secTime++}`
+    : `${minTime}:${secTime++}`;
+  // увеличение значения минут на 1, если дошло до 60 секунд
+  // и сброс значения минут
+  if (secTime == 60) {
+    ++minTime;
+    secTime = 0;
+  }
 }
 
+// сброс значения таймера
+function resetTimer() {
+  timerText.innerHTML = "0:00";
+  minTime = 0;
+  secTime = 1;
+  isClockStarted = false;
+  clearInterval(timerClock);
+}
+
+// загрузка лёгкото лабиринта
+function loadEasy() {
+  drawMaze('easy_maze.png', 5, 5);
+  isFinished = false;
+  resetTimer();
+}
+
+// загрузка сложного лабиринта
 function loadHard() {
   drawMaze('maze.png', 268, 5);
+  isFinished = false;
+  resetTimer();
 }
